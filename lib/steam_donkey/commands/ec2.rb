a31761324e0bb74a1f90bb9bfbe44fe7c0dc7784
Peter Coulton <petercoulton@gmail.com>
2016-12-14 12:36:17 +0000

    Add optional argument FILTERS to ec2 list

--- lib/steam_donkey/commands/ec2.rb
+++ lib/steam_donkey/commands/ec2.rb
@@ -5,7 +5,7 @@ module SteamDonkey
     include Thor::Actions
     default_task :list
 
-    desc 'list', 'List ec2 instances'
+    desc 'list FILTERS', 'List ec2 instances'
     method_option :raw,      :aliases => '-r', :default => false,    :desc => 'Toggle to display headings', :type => :boolean
     method_option :render_headings, :aliases => '-h', :default => true, :desc => 'Toggle to display headings', :type => :boolean
     method_option :format,   :aliases => '-o', :default => 'pretty', :desc => 'Output format', :enum => %w(pretty raw)
@@ -13,15 +13,18 @@ module SteamDonkey
     method_option :filter_columns, :aliases => '-f', :default => 'State=running', :desc => 'Filters to apply'
     method_option :sort,     :aliases => '-s', :default => 'LaunchTime=desc', :desc => 'Columns to sort by'
     map :ls => :list
-    def list
+    def list(filters='')
       show_headings = options[:render_headings]
       format = options[:format]
       if options[:raw]
         show_headings = false
         format = 'raw'
       end
+
+      filters = options[:filter_columns] if filters.empty?
+
       # begin
-        instances = SteamDonkey::AWS::EC2::Listing.new(options[:filter_columns], options[:columns], options[:sort])
+        instances = SteamDonkey::AWS::EC2::Listing.new(filters, options[:columns], options[:sort])
         output = SteamDonkey::Cli::Output.new(show_headings, format)
         output.render(instances.column_labels, instances.list)
       # rescue Exception => msg
